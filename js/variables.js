cbb5e8ef12e37bfd98db7c0a640ebb7cf1c196d1
Pilar Gomez <pilarg@gmail.com>
2022-06-22 22:49:43 -0300

    eventos

--- js/variables.js
+++ js/variables.js
@@ -2,5 +2,16 @@ const IVA = 1.21
 const productosTienda = []
 const carrito = []
 
+const miCompra = [
+    { nombre: "SOFÁ MODULAR CAMARO", precio: 245000},
+    { nombre: "BUTACA FUSION", precio: 85750},
+    { nombre: "LAMPARA WHITE", precio: 76850},
+    { nombre: "MESA C", precio: 97700},  
+]
+
+const total = miCompra.reduce((acc, el) => acc + el.precio, 0)
+console.log(total)
+
+
 
 
---
+++ variablesNewsletter.js
@@ -0,0 +1,6 @@
+const inputNombre = document.querySelector("#inputNombre")
+const inputTelefono = document.querySelector("#inputTelefono")
+const inputEmail = document.querySelector("#inputEmail")
+const btnSubmit = document.querySelector("#submit")
+
+let datosDeInput = ""
